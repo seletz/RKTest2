63b705f71a43a041c1b6b34f1d102625cd3deff2
Stefan Eletzhofer <se@nexiles.de>
2011-02-25 11:34:53 +0100

    Project setup.

---
+++ Classes/RKTest2AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  RKTest2AppDelegate.h
+//  RKTest2
+//
+//  Created by Stephan Eletzhofer on 25.02.11.
+//  Copyright 2011 Nexiles GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#include <RestKit/RestKit.h>
+
+@interface RKTest2AppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
